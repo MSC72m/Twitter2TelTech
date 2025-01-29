03a25c8da8541eecbc4aef671ba2f4d05ce8c807
Pourya Samimi <neverdiegenius@gmail.com>
2025-01-29 14:07:48 +0330

    Reorganize imports and clean up extra spaces function bodies

--- src/services/telegram/handlers/handler.py
+++ src/services/telegram/handlers/handler.py
@@ -1,31 +1,40 @@
-import sys, os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../../..")
-from telebot.async_telebot import AsyncTeleBot
+import os
+import sys
+
 from telebot import types
+from telebot.async_telebot import AsyncTeleBot
 from telebot.types import Message
+
 from src.database.db import get_session
 from src.database.models.models import Category
 from src.database.repositories.repositories import CategoryRepository
 from src.services.telegram.keyboards.keyboard import category_list_button
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../../..")
 
-async def handler_start_command(bot: AsyncTeleBot, message: Message) -> None :
+
+
+async def handler_start_command(bot: AsyncTeleBot, message: Message) -> None:
     markup = types.ReplyKeyboardMarkup(row_width=1)
     item_button = types.KeyboardButton('sign up')
     markup.add(item_button)
     await bot.send_message(message.from_user.id, "Choose one letter:", reply_markup=markup)
 
-async def handler_sign_up(bot: AsyncTeleBot, message: Message) -> None :
+
+async def handler_sign_up(bot: AsyncTeleBot, message: Message) -> None:
     async with get_session() as session:
         category_repo = CategoryRepository(Category, session)
         category_list = await category_repo.get_all_category_info()
         markup = category_list_button(category_list)
     await bot.send_message(message.from_user.id,"Choose category" ,reply_markup=markup)
 
-async def handler_text_command(bot: AsyncTeleBot, message: Message) -> None :
+
+async def handler_text_command(bot: AsyncTeleBot, message: Message) -> None:
     await bot.reply_to(message, "Wrong Command")
 
-async def handler_help_command(bot: AsyncTeleBot, message: Message) -> None :
+
+async def handler_help_command(bot: AsyncTeleBot, message: Message) -> None:
     await bot.reply_to(message, "Help list command")
+
 
 async def handle_callback_query(bot, call):
     if call.data == "option1":
