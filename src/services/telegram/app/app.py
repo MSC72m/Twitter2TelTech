61d4d4d5708637d67c8f3b4e5e8f244a3bca2965
Pourya Samimi <neverdiegenius@gmail.com>
2025-02-03 03:11:11 +0330

    refactor: add logger to better troubleshooting

--- src/services/telegram/app/app.py
+++ src/services/telegram/app/app.py
@@ -1,12 +1,20 @@
 import os
 from dotenv import load_dotenv
+import logging
 from telebot.async_telebot import AsyncTeleBot
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
 
 def generate_app() -> AsyncTeleBot :
     load_dotenv()
     BOT_TOKEN = os.getenv("BOT_TOKEN")
     if not BOT_TOKEN:
+        logger.error("BOT_TOKEN is not set on the environment variables.")
         raise ValueError("BOT_TOKEN is not set in the environment variables.")
 
     bot = AsyncTeleBot(BOT_TOKEN)
+
+    logger.info("Bot instance created successfully.")
     return bot
