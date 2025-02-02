b811dc9c587ce2d47f89205fa5d5fd6fb1a92f62
Pourya Samimi <neverdiegenius@gmail.com>
2025-02-02 17:33:35 +0330

    feat: add message handlers for Follow Category and Follow Account commands and store user selection in redis

--- src/services/telegram/bot.py
+++ src/services/telegram/bot.py
@@ -1,14 +1,21 @@
 import logging
-from pyexpat.errors import messages
 from telebot.types import Message
 from telebot.async_telebot import AsyncTeleBot
 from app.app import generate_app
-from handlers.handler import handler_start_command, handler_sign_up
+from handlers.handler import handler_start_command, handler_follow_category, handler_follow_account
 import asyncio
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../..")
+from src.cache.cache import get_cache_session
+from src.cache.payload import RedisPayload
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
+
+# Configure redis
+rdb = get_cache_session()
 
 # Create bot instance
 bot : AsyncTeleBot = generate_app()
@@ -18,9 +25,29 @@ bot : AsyncTeleBot = generate_app()
 async def start_command(message: Message):
     await handler_start_command(bot, message)
 
-@bot.message_handler(func=lambda message: True if message.text == "sign up" else False)
-async def sign_up(message: Message):
-    await handler_sign_up(bot, message)
+@bot.message_handler(func=lambda message: True if message.text == "Follow Category" else False)
+async def follow_category(message: Message):
+    payload = RedisPayload(
+            twitter_account = False,
+            twitter_category = True,
+            account = "",
+            category = 0,
+            )
+    status = rdb.set(message.from_user.id, payload.model_dump_json())
+    logger.info(f"{status} {message.from_user.id} set {payload}")
+    await handler_follow_category(bot, message)
+
+@bot.message_handler(func=lambda message: True if message.text == "Follow Account" else False)
+async def follow_account(message: Message):
+    payload = RedisPayload(
+            twitter_account = True,
+            twitter_category = False,
+    account = "",
+            category = 0,
+            )
+    status = rdb.set(message.from_user.id, payload.model_dump_json())
+    logger.info(f"{status} {message.from_user.id} set {payload}")
+    await handler_follow_account(bot, message)
 
 async def main():
     await bot.infinity_polling()
