2f50a3a5faae360602a318420c15b93c70e0cdfc
Pourya Samimi <neverdiegenius@gmail.com>
2025-02-03 03:03:52 +0330

    refactor: bot token error handling

--- src/services/telegram/app/app.py
+++ src/services/telegram/app/app.py
@@ -5,5 +5,8 @@ from telebot.async_telebot import AsyncTeleBot
 def generate_app() -> AsyncTeleBot :
     load_dotenv()
     BOT_TOKEN = os.getenv("BOT_TOKEN")
+    if not BOT_TOKEN:
+        raise ValueError("BOT_TOKEN is not set in the environment variables.")
+
     bot = AsyncTeleBot(BOT_TOKEN)
     return bot
