fa70df58874659bb3a74c3aec52a588c186a7069
Pourya Samimi <neverdiegenius@gmail.com>
2025-02-02 17:27:30 +0330

    feat: add handler for following Twitter accounts

--- src/services/telegram/handlers/handler.py
+++ src/services/telegram/handlers/handler.py
@@ -1,32 +1,40 @@
 import os
 import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../../..")
 
 from telebot import types
 from telebot.async_telebot import AsyncTeleBot
 from telebot.types import Message
 
 from src.database.db import get_session
-from src.database.models.models import Category
-from src.database.repositories.repositories import CategoryRepository
-from src.services.telegram.keyboards.keyboard import category_list_button
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../../..")
+from src.database.models.models import Category, TwitterAccount
+from src.database.repositories.repositories import CategoryRepository, TwitterAccountRepository
+from src.services.telegram.keyboards.keyboard import category_list_button, account_list_button
+from src.cache.cache import get_cache_session
 
 
 
 async def handler_start_command(bot: AsyncTeleBot, message: Message) -> None:
     markup = types.ReplyKeyboardMarkup(row_width=1)
-    item_button = types.KeyboardButton('sign up')
-    markup.add(item_button)
+    account_button = types.KeyboardButton('Follow Account')
+    category_button = types.KeyboardButton('Follow Category')
+    markup.add(account_button, category_button)
     await bot.send_message(message.from_user.id, "Choose one letter:", reply_markup=markup)
 
 
-async def handler_sign_up(bot: AsyncTeleBot, message: Message) -> None:
+async def handler_follow_category(bot: AsyncTeleBot, message: Message) -> None:
     async with get_session() as session:
         category_repo = CategoryRepository(Category, session)
         category_list = await category_repo.get_all_category_info()
         markup = category_list_button(category_list)
-    await bot.send_message(message.from_user.id,"Choose category" ,reply_markup=markup)
+    await bot.send_message(message.from_user.id, "Choose category" ,reply_markup=markup)
 
+async def handler_follow_account(bot: AsyncTeleBot, message: Message) -> None:
+    async with get_session() as session:
+        account_repo = TwitterAccountRepository(TwitterAccount, session)
+        account_list = await account_repo.get_twitter_accounts()
+        markup = account_list_button(account_list)
+    await bot.send_message(message.from_user.id, "Choose account", reply_markup=markup)
 
 async def handler_text_command(bot: AsyncTeleBot, message: Message) -> None:
     await bot.reply_to(message, "Wrong Command")
@@ -37,7 +45,4 @@ async def handler_help_command(bot: AsyncTeleBot, message: Message) -> None:
 
 
 async def handle_callback_query(bot, call):
-    if call.data == "option1":
-        bot.answer_callback_query(call.id, "You chose Option 1!")
-    elif call.data == "option2":
-        bot.answer_callback_query(call.id, "You chose Option 2!")
+    pass
