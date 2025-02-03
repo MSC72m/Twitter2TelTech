2c8b8038ded3e6616e414f4a50d71bd956980667
Pourya Samimi <neverdiegenius@gmail.com>
2025-02-03 15:42:28 +0330

    fix: fix some bugs about singleton

--- src/cache/cache.py
+++ src/cache/cache.py
@@ -1,16 +1,16 @@
 import logging
 import os
-from sys import exception
 import redis
 from dotenv import load_dotenv
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
+_redis_instance: redis.Redis|None = None
 
 def get_cache_session() -> redis.Redis:
     load_dotenv()
-    host = os.getenv("REDIS_HOST", "127.0.0.1")
+    host = os.getenv("REDIS_HOST", "localhost")
     port = int(os.getenv("REDIS_PORT", "6379"))
     global _redis_instance
     if _redis_instance is not None:
@@ -19,7 +19,7 @@ def get_cache_session() -> redis.Redis:
     try:
         _redis_instance = redis.Redis(host=host, port=port, db=0)
         _redis_instance.ping()
-        logger.debug("REDIS session created successfully")
+        logger.info("REDIS session created successfully")
         return _redis_instance
     except Exception as e:
         logger.error(f"Failed to create Redis session: {e}")
