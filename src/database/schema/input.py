4d3b7a2e2280ead097157a496cb266e89936999a
Pourya Samimi <neverdiegenius@gmail.com>
2025-02-05 13:04:31 +0330

    refactor: change and add pydantic model for data storage and readability

--- src/database/schema/input.py
+++ src/database/schema/input.py
@@ -1,9 +1,16 @@
 from pydantic import BaseModel
 
-class SubscribeUserAccount(BaseModel):
+class SubscribeAccount(BaseModel):
     telegram_id: int
     daily_digest: bool
     weekly_digest: bool
+    twitter_account: str
+
+class SubscribeCategory(BaseModel):
+    telegram_id: int
+    daily_digest: bool
+    weekly_digest: bool
+    twitter_category: int
 
 class GetTelegramUser(BaseModel):
     telegram_id: int
