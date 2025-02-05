7a52aee0ddc34c5edd59a5694366f0e919fb41b3
Pourya Samimi <neverdiegenius@gmail.com>
2025-02-05 13:02:37 +0330

    feat: Create handler for get user selection and save

--- src/services/telegram/handlers/handler.py
+++ src/services/telegram/handlers/handler.py
@@ -4,18 +4,18 @@ import sys
 
 import redis
 
-from src.cache.payload import RedisPayload
-
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../../..")
 
 from telebot import types
 from telebot.async_telebot import AsyncTeleBot
-from telebot.types import Message
+from telebot.types import Message, CallbackQuery
 
 from src.database.db import get_session
-from src.database.models.models import Category, TwitterAccount
-from src.database.repositories.repositories import CategoryRepository, TwitterAccountRepository
-from src.services.telegram.keyboards.keyboard import category_list_button, account_list_button
+from src.cache.payload import RedisPayload
+from src.database.models.models import Category, TwitterAccount, User
+from src.database.repositories.repositories import CategoryRepository, TwitterAccountRepository, UserRepository
+from src.services.telegram.keyboards.keyboard import category_list_button, account_list_button, time_button
+from src.database.schema.input import SubscribeAccount, SubscribeCategory
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -26,7 +26,6 @@ async def handler_start_command(bot: AsyncTeleBot, message: Message) -> None:
     category_button = types.KeyboardButton('Follow Category')
     markup.add(account_button, category_button)
     await bot.send_message(message.from_user.id, "Choose one letter:", reply_markup=markup)
-
 
 async def handler_follow_category(bot: AsyncTeleBot, message: Message, rdb: redis.Redis) -> None:
     payload = RedisPayload(
@@ -63,10 +62,67 @@ async def handler_follow_account(bot: AsyncTeleBot, message: Message, rdb: redis
 async def handler_text_command(bot: AsyncTeleBot, message: Message) -> None:
     await bot.reply_to(message, "Wrong Command")
 
-
 async def handler_help_command(bot: AsyncTeleBot, message: Message) -> None:
     await bot.reply_to(message, "Help list command")
 
+async def handle_callback_query(bot: AsyncTeleBot, call: CallbackQuery, rdb: redis.Redis) -> None:
+    key = rdb.getdel(str(call.from_user.id))
+    if key:
+        payload = RedisPayload.model_validate_json(key)
+        if payload.twitter_account:
+            payload.account = call.data
+            status = rdb.set(call.from_user.id, payload.model_dump_json())
+            logger.info(f"{status} User {call.from_user.username} (ID: {call.from_user.id}) set {payload}")
+            markup = time_button()
+            await bot.send_message(call.from_user.id, "choose time", reply_markup=markup)
 
-async def handle_callback_query(bot, call):
-    pass
+        elif payload.twitter_category:
+            payload.category = call.data
+            status = rdb.set(call.from_user.id, payload.model_dump_json())
+            logger.info(f"{status} User {call.from_user.username} (ID: {call.from_user.id}) set {payload}")
+            markup = time_button()
+            await bot.send_message(call.from_user.id, "choose time", reply_markup=markup)
+    elif call.data == "daily" and key:
+        payload = RedisPayload.model_validate_json(key)
+        async with get_session as session:
+            repo = UserRepository(User, session)
+            if payload.twitter_account:
+                user_input = SubscribeAccount(
+                    telegram_id=call.from_user.id,
+                    daily_digest=True,
+                    weekly_digest=False,
+                    twitter_account=payload.account
+                )
+                await repo.subscribe_user_account(user_input)
+                await bot.send_message(call.from_user.id, "OK")
+            elif payload.twitter_category:
+                user_input = SubscribeCategory(
+                    telegram_id=call.from_user.id,
+                    daily_digest=True,
+                    weekly_digest=False,
+                    twitter_category=payload.category
+                )
+                await repo.subscribe_user_category(user_input)
+                await bot.send_message(call.from_user.id, "OK")
+    elif call.data == "weekly" and key:
+        payload = RedisPayload.model_validate_json(key)
+        async with get_session as session:
+            repo = UserRepository(User, session)
+            if payload.twitter_account:
+                user_input = SubscribeAccount(
+                    telegram_id=call.from_user.id,
+                    daily_digest=False,
+                    weekly_digest=True,
+                    twitter_account=payload.account
+                )
+                await repo.subscribe_user_account(user_input)
+                await bot.send_message(call.from_user.id, "OK")
+            elif payload.twitter_category:
+                user_input = SubscribeCategory(
+                    telegram_id=call.from_user.id,
+                    daily_digest=False,
+                    weekly_digest=True,
+                    twitter_category=payload.category
+                )
+                await repo.subscribe_user_category(user_input)
+                await bot.send_message(call.from_user.id, "OK")
